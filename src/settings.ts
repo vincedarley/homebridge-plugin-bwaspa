076bc34edc0adc7c85c4686e6ecb81b3310b7571
justinfanok <justinfanok@gmail.com>
2020-08-19 13:55:39 -0400

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,11 +1,11 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'Balboa-Spa';
+export const PLATFORM_NAME = 'Balboa-Spa'
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-balboa-spa';
+export const PLUGIN_NAME = 'homebridge-balboa-spa'
 
 export const VERSION = '2.0.15'
